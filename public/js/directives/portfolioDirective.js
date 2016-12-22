55b146651fcd38e6405a95c16015a45e738d626d
elliot richardson <ear78@aol.com>
2016-12-22 01:42:51 -0600

    worked on functionality for portfolio section

--- public/js/directives/portfolioDirective.js
+++ public/js/directives/portfolioDirective.js
@@ -2,6 +2,11 @@ angular.module('myApp')
     .directive('portfolioDirective', function(){
         return {
             restrict: 'E',
-            templateUrl: '../../views/directives/portfolioDirective.html'
+            templateUrl: '../../views/directives/portfolioDirective.html',
+            link: function(){
+                $('.box1, .box2').hover(function(){
+                    $(this).find('.project-info-container').toggleClass('info-active');
+                })
+            }
         }
     })
