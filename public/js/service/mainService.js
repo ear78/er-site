a06d11334363761f22bc84950e8e7ac3c8727881
elliot richardson <ear78@aol.com>
2016-12-29 13:48:07 -0600

    add some animations and worked on mobile portfolio section dynamically loading pics from database

--- public/js/service/mainService.js
+++ public/js/service/mainService.js
@@ -3,34 +3,40 @@ angular.module('myApp')
 
         this.portfolio = [
             {
-            img: './img/ersite802x694.jpg',
-            name: 'Elliot Richardson Site',
-            tech: 'HTML, CSS, Javascript, Angular, Jquery, Gulp, Sass, Node.js'
+                img: './img/ersite802x694.jpg',
+                name: 'Elliot Richardson Site',
+                tech: 'HTML, CSS, Javascript, Angular, Jquery, Gulp, Sass, Node.js',
+                images: ['./img/ersite802x694.jpg', './img/ersitefooterthumb.jpg', './img/ersitemobilethumb2.jpg', './img/ersiteaboutmobilethumb.jpg']
             },
             {
-            img: './img/rodesk802x694.jpg',
-            name: 'Rodesk Site Clone',
-            tech: 'HTML, CSS, Javascript, Angular, Jquery, Sketch'
+                img: './img/rodesk802x694.jpg',
+                name: 'Rodesk Site Clone',
+                tech: 'HTML, CSS, Javascript, Angular, Jquery, Sketch',
+                images: ['./img/ersite802x694.jpg', './img/rodesk802x694.jpg', './img/Beoplay802x694.jpg', './img/optimalins802x694.jpg']
             },
             {
-            img: './img/Beoplay802x694.jpg',
-            name: 'Beoplay Site Clone',
-            tech: 'HTML, CSS, Javascript, Angular, Jquery, Gulp, Sass, Node.js'
+                img: './img/Beoplay802x694.jpg',
+                name: 'Beoplay Site Clone',
+                tech: 'HTML, CSS, Javascript, Angular, Jquery, Gulp, Sass, Node.js',
+                images: ['./img/ersite802x694.jpg', './img/rodesk802x694.jpg', './img/Beoplay802x694.jpg', './img/optimalins802x694.jpg']
             },
             {
-            img: './img/optimalins802x694.jpg',
-            name: 'Optimal Insurance Site',
-            tech: 'HTML, CSS, SquareSpace CMS, Affinity Designer'
+                img: './img/optimalins802x694.jpg',
+                name: 'Optimal Insurance Site',
+                tech: 'HTML, CSS, SquareSpace CMS, Affinity Designer',
+                images: ['./img/ersite802x694.jpg', './img/rodesk802x694.jpg', './img/Beoplay802x694.jpg', './img/optimalins802x694.jpg']
             },
             {
-            img: './img/emp802x694.jpg',
-            name: 'Empowered Emp Site',
-            tech: 'HTML, CSS, Responsive design, Affinity Designer'
+                img: './img/emp802x694.jpg',
+                name: 'Empowered Emp Site',
+                tech: 'HTML, CSS, Responsive design, Affinity Designer',
+                images: ['./img/ersite802x694.jpg', './img/rodesk802x694.jpg', './img/Beoplay802x694.jpg', './img/optimalins802x694.jpg']
             },
             {
                 img: './img/slice8.jpg',
                 name: 'Lyft Site Clone',
-                tech: 'HTML, CSS, Javascript, React, Webpack, Bootstrap'
+                tech: 'HTML, CSS, Javascript, React, Webpack, Bootstrap',
+                images: ['./img/ersite802x694.jpg', './img/rodesk802x694.jpg', './img/Beoplay802x694.jpg', './img/optimalins802x694.jpg']
             }
         ];
 
