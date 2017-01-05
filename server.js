b20111b130bd609dc1fc0cf2467d20a164cfea61
elliot richardson <ear78@aol.com>
2017-01-05 16:23:14 -0600

    changed port for heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var session = require('express-session');
 var cors = require('cors');
 var massive = require('massive');
 var config = require('./config.json');
+var port = process.env.PORT || 4000;
 
 var app = module.exports = express();
 var db = massive.connectSync({
@@ -19,6 +20,6 @@ app.use(express.static("./public"));
 
 
 
-app.listen(config.port, function(){
-    console.log('port is on', config.port);
+app.listen(port, function(){
+    console.log('port is on', port);
 })
