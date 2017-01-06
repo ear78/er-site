37c257f537c7dd332cba326906b994a5d344dfa7
elliot richardson <ear78@aol.com>
2017-01-06 01:22:01 -0600

    m

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var cors = require('cors');
-var massive = require('massive');
+// var bodyParser = require('body-parser');
+// var session = require('express-session');
+// var cors = require('cors');
+// var massive = require('massive');
 var port = 5050;
 
 var app = express();
@@ -12,10 +12,10 @@ var app = express();
 
 
 app.use(express.static("./public"));
-app.set('port', (process.env.PORT || 5050));
+// app.set('port', (process.env.PORT || 5050));
 
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
 // app.use(express.static("./public"));
 
