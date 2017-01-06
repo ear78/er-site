031a71ccfc470287a32cdb4956535e75e9d9f85e
elliot richardson <ear78@aol.com>
2017-01-06 15:15:04 -0600

    modified

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var port = 5050;
-
-var app = express();
-
-
-
-app.use(express.static("./public"));
-app.set('port', (process.env.PORT || 5050));
-
-app.use(cors());
-app.use(bodyParser.json());
-
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var cors = require('cors');
+// var port = 5050;
+//
+// var app = express();
+//
+//
+//
 // app.use(express.static("./public"));
+// app.set('port', (process.env.PORT || 5050));
+//
+// // app.use(cors());
+// app.use(bodyParser.json());
+//
+//
+//
+//
+//
+// app.listen(port, function(){
+//     console.log('port is on', port);
+// })
 
 
+// var express = require('express');
+//
+// var port = process.env.PORT || 3000;
+// var app = express();
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// app.all('*', function(req, res, next) {
+//     res.sendFile('/public/index.html', { root: __dirname });
+// });
+//
+// app.listen(port, function(err){
+//   console.log('connected at ', port);
+// });
 
-
-app.listen(port, function(){
-    console.log('port is on', port);
-})
+var express = require('express')
+var port = process.env.PORT || 5050
+var app = express()
+app
+.use(express.static(__dirname + '/public'))
+.listen(port, function() { console.log('listening on', port) })
